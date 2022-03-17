ff93d0a7cb18c59ed0df2bbfb4043144383b5fcb
Samson Hailu <alxsamsonhailu@gmail.com>
2022-03-17 19:44:08 +0300

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9 but not 2 and 4
+ *
+ * Return: The product of a and b
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		if (c != '2' && c != '4')
+			_putchar(c);
+
+	_putchar('\n');
+}

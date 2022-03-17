cb456236561eb719fc648fa5c65b1bb6cd26a229
Samson Hailu <alxsamsonhailu@gmail.com>
2022-03-17 19:46:47 +0300

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar('1');
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}

af81900df6600cc589b8efa94e5b6a9ffcd715f6
Samson Hailu <alxsamsonhailu@gmail.com>
2022-03-17 19:40:19 +0300

    create Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integer
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
